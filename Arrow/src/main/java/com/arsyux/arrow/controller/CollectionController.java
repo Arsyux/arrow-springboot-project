94bd7f344af8990ea31853994655c176518c8e3a
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-04-08 01:51:59 +0900

    Update CollectionController.java

--- Arrow/src/main/java/com/arsyux/arrow/controller/CollectionController.java
+++ Arrow/src/main/java/com/arsyux/arrow/controller/CollectionController.java
@@ -1,13 +1,24 @@
 package com.arsyux.arrow.controller;
 
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Base64;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +32,9 @@ import com.arsyux.arrow.service.CollectionService;
 public class CollectionController {
 	
 	// 파일이 저장되는 경로
-	private static final String FILE_PATH = "C:/NewFolder";
+	@Value("${FILE_PATH}")
+	private String FILE_PATH;
+	
 	
 	//@Autowired
 	//private PostService postService;
@@ -62,11 +75,19 @@ public class CollectionController {
 	// 전시 정보 페이지
 	@GetMapping("/exhibition/view/CollectionInfo/{encryptedCodeName}")
 	public String getCollectInfo(Model model,@PathVariable("encryptedCodeName") String encryptedCode) {
-		 
 		encryptedCode = new String(Base64.getDecoder().decode(encryptedCode));
 	
 		List<CollectionsVO> collectionList = collectionService.selectOneCollect(encryptedCode);
-		
+		try {
+			BufferedImage image;
+			//로컬 파일을 사용하는 경우 
+			File imageFile = new File(FILE_PATH , "ad56a20b-422f-4d81-a0f5-b5772e2c70b1.jpg");
+			image = ImageIO.read(imageFile);
+			System.out.println("image"+image);
+			model.addAttribute("image", image);
+		 }catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 		
 		model.addAttribute("collect", collectionList);
